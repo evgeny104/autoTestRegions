aeb338c8a383f3f8d50f8c198d90f14bfbc84836
evgeny104 <nazawka88@gmail.com>
2024-03-21 19:46:09 +0300

    Checking country codes: 'kg'

--- advanced_checks.py
+++ advanced_checks.py
@@ -1,20 +1,42 @@
 import requests
 
 
-def test_country_code_kg():
-    # Код страны по фильтру "ru"
+def country_code_ru():
     url = 'https://regions-test.2gis.com/1.0/regions?country_code=ru'
     response = requests.get(url)
-    # Проверка успешности запроса
     assert response.status_code == 200
-    # Получение данных в формате JSON
     data = response.json()
-    # Создание списка li для значения country_code
-    li = [item['country']['code'] for item in data['items']]
-    # Проверка, что количество значений "kg" равно длине списка li
+    li = [item['country']['code']
+          for item in data['items']]
     assert li.count('ru') == len(li)
     print("pass")
 
 
-# Запуск теста
-test_country_code_kg()
+# run check functions country_code_ru():
+country_code_ru()
+
+
+def country_code_kg():
+    url = 'https://regions-test.2gis.com/1.0/regions?country_code=kg'
+    request = requests.get(url)
+    if request.status_code == 200:
+        data = request.json()
+        country_codes = [item['country']['code']
+                         for item in data['items']]
+        return country_codes
+    else:
+        # If the status code is != 200, display an error message
+        print(f"Error while receiving data. Status code: {request.status_code}")
+        return None
+
+
+result = country_code_kg()
+count_ru = result.count('kg')
+
+if count_ru == len(result):
+    print("All values in the list = 'kg'")
+else:
+    print(f"{result}\nNot all values in the list = 'kg'")
+
+# run check functions country_code_kg():
+country_code_kg()
